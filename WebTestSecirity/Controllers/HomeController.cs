c1bf1697bc9d5208eb9550a1fd03b3c77370e4a5
Volodymyr Novak <novakvova@gmail.com>
2020-08-17 11:47:53 +0300

    Show list user in home page

--- WebTestSecirity/Controllers/HomeController.cs
+++ WebTestSecirity/Controllers/HomeController.cs
@@ -1,16 +1,50 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebTestSecirity.Models;
 
 namespace WebTestSecirity.Controllers
 {
     public class HomeController : Controller
     {
+
+        private ApplicationUserManager _userManager;
+
+        public HomeController()
+        {
+        }
+
+        public HomeController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        
         public ActionResult Index()
         {
-            return View();
+            var list = UserManager.Users
+                .Select(u=>new UserVM 
+                {
+                    Id=u.Id,
+                    Email=u.Email
+                }).ToList();
+
+            return View(list);
         }
 
         public ActionResult About()
--- WebTestSecirity/Models/AccountViewModels.cs
+++ WebTestSecirity/Models/AccountViewModels.cs
@@ -66,17 +66,17 @@ namespace WebTestSecirity.Models
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "Електронна пошта")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Підтвердження пароля")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
