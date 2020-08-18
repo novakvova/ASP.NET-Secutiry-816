a9e7b494af21f25e3891259e0c2746183284322e
Volodymyr Novak <novakvova@gmail.com>
2020-08-18 09:36:17 +0300

    Add table UserProfile

--- WebTestSecirity/Models/IdentityModels.cs
+++ WebTestSecirity/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using WebTestSecirity.Entities;
 
 namespace WebTestSecirity.Models
 {
@@ -16,6 +17,8 @@ namespace WebTestSecirity.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public virtual UserProfile UserProfile { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -29,5 +32,7 @@ namespace WebTestSecirity.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
     }
 }
