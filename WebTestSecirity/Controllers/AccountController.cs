b4ab2c82a660c6eba2846c6f0e0a47ec347a6c2a
STORYM <ssashkaa01@gmail.com>
2020-08-19 11:45:10 +0300

    add in login form check email confirmed

--- WebTestSecirity/Controllers/AccountController.cs
+++ WebTestSecirity/Controllers/AccountController.cs
@@ -90,6 +90,14 @@ namespace WebTestSecirity.Controllers
                 return View(model);
             }
 
+            ApplicationUser u = UserManager.FindByEmail(model.Email);
+            
+            if(!u.EmailConfirmed)
+            {
+                ModelState.AddModelError("", "Email not confirmed");
+                return View(model);
+            }
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
